a8cd44b82c929f0afee40a42263c932a1f98be2e
bteger508 <56287781+bteger508@users.noreply.github.com>
2021-04-28 14:35:39 -0400

    Finish read_position(MMSI) function

--- src/dao.js
+++ src/dao.js
@@ -4,33 +4,16 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
-stub = false
-
 // Database Name
 const dbName = 'AISTestData';
 
 
-// A dummy query for reference
-exports.query = async function (){
-	const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
-	
-	try {
-        await client.connect();
-        
-	    const vessels = client.db(dbName).collection('vessels')
-		var docs = await vessels.aggregate([
-		    {$match: {IMO: 1000007}}, 
-		    {$project: {_id:0, Name: 1}}
-		]).toArray();
-    
-    // return the last former name from the list
-    return docs[0].Name
-
-	} finally { client.close(); }
-}
-
 // Insert an array of json AIS documents into the mongo database
-exports.insert = async function(data){
+exports.insert = async function(data, stub = false){
+    
+    // If function is called in stub mode, return the array of JSON AIS docs passed as an argument
+    if (stub) { return data }
+    
 	const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
 	
 	try {
@@ -42,5 +25,36 @@ exports.insert = async function(data){
 	    return { "Inserted": out.insertedCount};
 	} finally {
 	    await client.close()
+	}
+}
+
+
+// Retrieve position information for a given MMSI 
+exports.read_position = async function(mmsi, stub = false){
+	const client = new MongoClient('mongodb://localhost:27017', {useUnifiedTopology: true});
+	
+	// If function is called in stub mode, return the MMSI passed as an argument
+	if (stub) { return mmsi }
+	
+	// Else, execute the query
+	try {
+	    await client.connect();
+	    const ais_messages = client.db(dbName).collection('ais_messages')
+	    let position = await ais_messages.aggregate([{$match: {"MMSI": mmsi}},
+	                    {$project: {_id:0, MMSI:1, "Position.coordinates": 1}},
+	                    {$sort: {Timestamp: -1}},
+	                    {$limit: 1}]).toArray()
+	                    
+	    let imo = await ais_messages.aggregate([{$match: {IMO: {$gte: 0}, MMSI: mmsi}}, {$project: {_id:0, IMO:1}}, {$sort: {Timestamp: -1}}, {$limit:1}]).toArray()
+	    
+         if (imo.length === 0) {
+            console.log("checking vessel collection for a matching IMO")
+            const vessels = client.db(dbName).collection('vessels')
+            imo = await vessels.aggregate([{$match: {IMO: {$gte: 0}, MMSI: mmsi}}, {$project: {_id:0, IMO:1}}, {$limit:1}]).toArray()
+         }
+        
+	    return {"MMSI": position[0].MMSI, 'Lat': position[0].Position.coordinates[0], 'Long': position[0].Position.coordinates[0], 'IMO': imo[0].IMO}
+	} finally {
+	    client.close()
 	}
 }
--- test/test.js
+++ test/test.js
@@ -3,17 +3,46 @@ const assert = chai.assert
 const dao = require('../src/dao')
 const sample_input = require('../data/sample_input.json')
 
-describe('execute query', () => {
+
+/*
+*  Unit Tests
+*/
+
+// insert() is called with proper parameters
+describe('insert() is called with an array of JSON AIS documents', () => {
     it('', async () => {
-        const name = await dao.query();
-        assert.equal(name, 'Athina');
+        const parameter = await dao.insert(sample_input, true)
+        assert.deepEqual(parameter, sample_input)
     })
 });
 
+
 // Unit test for insert() method using sample AIS json docs
-describe('execute insert', () => {
+describe('insert() 500 sample AIS JSON docs into the mongo DB', () => {
     it('', async () => {
         const response = await dao.insert(sample_input);
         assert.deepEqual(response, {"Inserted": 500});
+    })
+});
+
+// read_position() is called with proper parameter
+describe('read_postition is called with a 9 digit integer MMSI', () => {
+    it('', async () => {
+        var MMSI = 265177000
+        const parameter = await dao.read_position(MMSI, true)
+        assert.strictEqual(parameter, MMSI)
+    })
+});
+
+// read_position returns a position document in the correct format
+describe('read_postition returns a position doc in the correct format', () => {
+    it('', async () => {
+        var MMSI = 265177000
+        const position_report = await dao.read_position(MMSI)
+        console.log(position_report)
+        assert.property(position_report, "MMSI")
+        assert.property(position_report, "Lat")
+        assert.property(position_report, "Long")
+        assert.property(position_report, "IMO")
     })
 });
